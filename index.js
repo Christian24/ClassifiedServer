3955fd95ed32ebef029862d8ab3fdfa98dfd0ed3
Julian Handrup <JulianHandrup@outlook.de>
2016-05-20 16:00:08 +0200

    added routes and and (for testing) request handlers

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 var express = require("express");
 var app = express();
+/**
+ * Body Parser
+ */
 var bodyParser = require("body-parser");
-var send = require("./send.js");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+/**
+ * Require request handlers
+ */
+var send = require("./send.js");
+var register = require("./register.js");
+var login = require("./login.js");
+var pubkey = require("./pubkey.js");
+var messages = require("./messages.js");
+/**
+ * Default action for "/"
+ */
 app.get("/",function(request,response){
     response.write("Hallo Welt.");
     response.end();
 });
+/**
+ * routes and actions
+ */
 app.post("/send",send);
+app.post("/register",register);
+app.get("/login",login);
+app.get("/users/",pubkey);
+app.get("/messages",messages);
+
 
 app.listen("3000");
---
+++ login.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Julian on 20.05.2016.
+ */
+module.exports = function (request, response) {
+    response.end("Danke für deine Nachricht: " + request.body.title);
+
+}
