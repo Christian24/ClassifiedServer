2aef4bcbe7b344dbbeff6410ff7d7d6f79bb9c80
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-05-20 14:55:14 +0200

    Send route.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var send = require("./send.js");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/",function(request,response){
     response.write("Hallo Welt.");
     response.end();
 });
+app.post("/send",send);
+
 app.listen("3000");
