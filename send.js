6688d4729073c3aabc093463c1c7bdbf7d5011f3
Napfkuchen <sergeifladung@googlemail.com>
2016-05-27 16:09:23 +0200

    Funktionalität Nachricht senden erweitert

--- send.js
+++ send.js
@@ -1,7 +1,19 @@
 /**
- * Created by Christian on 20.05.2016.
+ * Created by Sergei on 27.05.2016.
  */
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./webwemser.db');
+var timestamp = Date.now();
 module.exports = function (request, response) {
+<<<<<<< HEAD
+	response.end("Danke für deine Nachricht: " + request.body.title);
+	var user = requests.params.user;
+	if(user) {
+		var sql = "SELECT pubkey_user from Users WHERE user = ?";
+		
+	}
+=======
     var username = request.body.name;
 response.end("Danke für deine Nachricht: " + username);
+>>>>>>> branch 'master' of https://github.com/Christian24/ClassifiedServer.git
 }
