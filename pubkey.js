9352a852e786aa3a5189f943f26d023f3f5d4bdb
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-05-27 11:22:58 +0200

    Pubkey get

--- pubkey.js
+++ pubkey.js
@@ -1,7 +1,28 @@
 /**
  * Created by Julian on 20.05.2016.
  */
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./webwemser.db');
 module.exports = function (request, response) {
-    response.end("Danke für deine Nachricht: " + request.body.title);
+    var user = request.params.user;
+    if(user) {
+        var sql = "SELECT pubkey_user from Users WHERE user = ?";
+        var statement = db.prepare(sql);
+        statement.get([user],function (error, row) {
+            if(error) {
+                console.log(error);
+                response.status(400).end("Sorry");
+            }else {
+                if(row) {
+                    response.status(200).send(JSON.stringify(row)).end();
+                } else {
+                    response.status(404).end("Sorry");
+                }
+            }
+        });
+    } else {
+        console.log("Daten nicht vollständig");
+        response.status(400).end("Sorry");
+    }
 
 }
