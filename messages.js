880c9875a5cd92db2db66f80d137d4ca2e8646e3
Julian Handrup <JulianHandrup@outlook.de>
2016-05-28 13:44:37 +0200

    messages.js now gets one envelope from the db, deletes the db-entry and returns it with code 200. On db error: code 500, no more messages: code 404. Also implemented checking of sig_utime and the timestamp.

--- messages.js
+++ messages.js
@@ -12,15 +12,30 @@ module.exports = function (request, response) {
     var timestamp = request.body.timestamp;
     var sig_utime = request.body.sig_utime;
 
+    
     if(user && timestamp && sig_utime)
     {
         var sig_utime_check = hash.update(user.toString()+timestamp.toString());
         var date = new Date();
-        if( (sig_utime == sig_utime_check) &&  (new Date().now()/1000 == timestamp))
+        if( (sig_utime == sig_utime_check) &&  !(new Date().now()/1000 < timestamp-(5*60)))
         {
-            var sql = "SELECT pubkey_user from Users WHERE user = ?";
-            var statement = db.prepare(sql);
-           // statement.get([user],function (error, row){}
+            var sql_get = "SELECT recipient,cipher,iv,key_recipient_enc,sig_recipient from Messages WHERE recipient = ? sort by timestamp asc limit 1";
+            var sql_delete = "DELETE * from Messages where id = ?";
+            var statement = db.prepare(sql_get);
+            statement.get([user],function (error, row) {
+                if(error) {
+                    console.log(error);
+                    response.status(500).end("Sorry");
+                }else {
+                    if(row) {
+                        var msg = row.id;
+                        db.run(sql_delete,[msg]);
+                        response.status(200).send(JSON.stringify(row)).end();
+                    } else {
+                        response.status(404).end("Sorry");
+                    }
+                }
+            });
 
         }else{
             response.status(400).end("Timestamp outdated or Signature not matching.")
@@ -29,12 +44,5 @@ module.exports = function (request, response) {
     }else{
         response.status(400).end("Provide missing information.")
     }
-
-
-
-
-
-
-    response.end("Danke für deine Nachricht: " + request.body.title);
 
 };
