8796d035b02ec7e951158cb5d49b04c69f98aee1
Napfkuchen <sergeifladung@googlemail.com>
2016-05-29 15:10:47 +0200

    Update send.js
    Solved merge conflict

--- send.js
+++ send.js
@@ -5,15 +5,9 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./webwemser.db');
 var timestamp = Date.now();
 module.exports = function (request, response) {
-<<<<<<< HEAD
 	response.end("Danke für deine Nachricht: " + request.body.title);
 	var user = requests.params.user;
 	if(user) {
 		var sql = "SELECT pubkey_user from Users WHERE user = ?";
-		
 	}
-=======
-    var username = request.body.name;
-response.end("Danke für deine Nachricht: " + username);
->>>>>>> branch 'master' of https://github.com/Christian24/ClassifiedServer.git
 }
