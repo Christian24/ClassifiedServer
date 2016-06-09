eea2f1d1b28627242fba1dcccfe99ff77e0738c2
Julian Handrup <JulianHandrup@outlook.de>
2016-06-09 18:33:45 +0200

    heroku deployment

--- db.js
+++ db.js
@@ -5,7 +5,7 @@
  * POSTGRESQL Connector.
  */
 var pg = require("pg");
-var conString="postgres://postgres:root@localhost:5432/webwemser";
+var conString="postgres://rbmokhcvrfyiky:N8Zf-l_hee3NCA4mqjxeQGlZ9p@ec2-54-163-239-12.compute-1.amazonaws.com:5432/d253prihss1ba3";
 module.exports={
     client:function(){
         return new pg.Client(conString);
