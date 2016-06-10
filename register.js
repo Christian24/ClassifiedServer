4b7eeb190bba416b65142091aac436c517ae8f08
Julian Handrup <JulianHandrup@outlook.de>
2016-06-10 11:42:49 +0200

    heroku

--- register.js
+++ register.js
@@ -13,33 +13,41 @@ module.exports = function (request, response) {
     var privkey_user_enc = request.body.privkey_user_enc;
     if(user && salt_masterkey && pubkey_user && privkey_user_enc) {
             console.log(salt_masterkey);
+            console.log(user);
 
+        client.connect(function(err, client, done) {
             /**
              * Check if the user already exists. If so, send back user defined error.(JH)
              */
-            client.query("Select Count 'user' as count_user from Users where user = $1 ", [user], function(error,result){
-                console.log("entered query");
-                if(error){
-                    console.log(error);
-                    response.status(500).end("Sorry");
-                } else {
-                    if(result.rows["count_user"] > 0){
-                        response.status(444).end("User already exists");
+            if (err) {
+                console.log(err);
+                response.status(500).end("Sorry");
+            }
+            if (client) {
+                client.query("Select Count user as count_user from Users where user = $1 ", [user], function (error, result) {
+                    console.log("entered query");
+                    if (error) {
+                        console.log(error);
+                        response.status(500).end("Sorry");
+                    } else {
+                        if (result.rows["count_user"] > 0) {
+                            response.status(444).end("User already exists");
+                        }
                     }
-                }
-            });
+                });
 
 
-            client.query("INSERT INTO Users VALUES($1,$2,$3,$4)",[user,salt_masterkey,pubkey_user,privkey_user_enc],function (error) {
-                if(error) {
-                    console.log(error);
-                    response.status(400).end("Sorry");
-                } else {
-                    console.log("User "+user+" successfully created.")
-                    response.status(200).end("Danke für deine Nachricht: ");
-                }
-            });
-
+                client.query("INSERT INTO Users VALUES($1,$2,$3,$4)", [user, salt_masterkey, pubkey_user, privkey_user_enc], function (error) {
+                    if (error) {
+                        console.log(error);
+                        response.status(400).end("Sorry");
+                    } else {
+                        console.log("User " + user + " successfully created.")
+                        response.status(200).end("Danke für deine Nachricht: ");
+                    }
+                });
+            }
+        })
 
     } else {
         console.log("Daten nicht vollständig");
