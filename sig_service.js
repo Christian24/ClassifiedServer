e92854d858af0a1fee7a9ab726b4ea0270b403be
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-06-10 14:53:18 +0200

    Sig_service test in send.js

---
+++ sig_service.js
@@ -0,0 +1,9 @@
+var crypto = require("crypto");
+
+/**
+ * Created by Christian on 10.06.2016.
+ * Creates a hash from envelope, timestamp, recipient and key
+ */
+module.exports = function (envelope,timestamp,recipient,key) {
+return crypto.createHash("sha256",key).update(envelope).update(timestamp).update(recipient);
+};
