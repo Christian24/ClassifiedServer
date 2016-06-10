3238ce63041ca0f1d4537f3b6200ded2a01631b9
Julian Handrup <JulianHandrup@outlook.de>
2016-06-10 15:47:56 +0200

    bugfix postgresql

--- db.js
+++ db.js
@@ -5,9 +5,17 @@
  * POSTGRESQL Connector.
  */
 var pg = require("pg");
-var conString="postgres://rbmokhcvrfyiky:N8Zf-l_hee3NCA4mqjxeQGlZ9p@ec2-54-163-239-12.compute-1.amazonaws.com:5432/d253prihss1ba3";
+//var conString="postgres://rbmokhcvrfyiky:N8Zf-l_hee3NCA4mqjxeQGlZ9p@ec2-54-163-239-12.compute-1.amazonaws.com:5432/d253prihss1ba3";
 module.exports={
-    client:function(){ 
-        return new pg.Client(conString);
+    client:function(){
+        return new pg.Client({
+            user: "rbmokhcvrfyiky",
+            password: "N8Zf-l_hee3NCA4mqjxeQGlZ9p",
+            database: "d253prihss1ba3",
+            port: 5432,
+            host: "ec2-54-163-239-12.compute-1.amazonaws.com",
+            ssl: true
+        });
+
     }
-}
+};
