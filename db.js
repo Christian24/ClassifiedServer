192755c58ec70a3e5843175e2b4c637c2d3b3a18
Julian Handrup <JulianHandrup@outlook.de>
2016-06-23 01:31:46 +0200

    why u no work heroku?

--- db.js
+++ db.js
@@ -4,8 +4,7 @@
 /**
  * POSTGRESQL Connector.
  */
-var pg = require("pg");
-var Pool = pg.Pool;
+var Pool = require("pg").Pool;
 //var conString="postgres://rbmokhcvrfyiky:N8Zf-l_hee3NCA4mqjxeQGlZ9p@ec2-54-163-239-12.compute-1.amazonaws.com:5432/d253prihss1ba3";
 var config = {
     user: "rbmokhcvrfyiky",
