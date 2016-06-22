d8fe715b9219b3f264c4e0e8ad04eead3b2c2f54
Julian Handrup <JulianHandrup@outlook.de>
2016-06-22 17:19:17 +0200

    added some commenting

--- messages.js
+++ messages.js
@@ -2,6 +2,7 @@
  * Created by Julian on 20.05.2016.
  */
 const crypto = require("crypto");
+//Create empty SHA256 Hash
 const hash = crypto.createHash("sha256");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./webwemser.db');
@@ -15,19 +16,25 @@ module.exports = function (request, response) {
     
     if(user && timestamp && sig_utime)
     {
+        // Update Hash-Content with given username and timestamp
         var sig_utime_check = hash.update(user.toString()+timestamp.toString());
         var date = new Date();
+        // If the two Hashes match the request is ok.
         if( (sig_utime == sig_utime_check) &&  !(new Date().now()/1000 < timestamp-(5*60)))
         {
+            //SQL-Query to get the oldest unread message
             var sql_get = "SELECT sender,cipher,iv,key_recipient_enc,sig_recipient from Messages WHERE recipient = ? sort by timestamp asc limit 1";
+            //Query to delete the oldest unread message
             var sql_delete = "DELETE * from Messages where id = ?";
             var statement = db.prepare(sql_get);
+            // Call statement with user as parameter
             statement.get([user],function (error, row) {
                 if(error) {
                     console.log(error);
                     response.status(500).end("Sorry");
                 }else {
                     if(row) {
+                        // delete the message we got from the database and send the results from the query to the application.
                         var msg_id = row.id;
                         db.run(sql_delete,[msg_id]);
                         response.status(200).send(JSON.stringify(row)).end();
@@ -45,4 +52,4 @@ module.exports = function (request, response) {
         response.status(400).end("Provide missing information.")
     }
 
-}; 
+};
